df6dece541187ed7d6789e3d200f812ed8bd2cd0
TatiCavichiole <tatianecavichiole@gmail.com>
2024-04-28 19:20:48 -0300

    update pesquisar.por.email

--- cypress/e2e/rarocrud.pesquisarUsuario.cy.js
+++ cypress/e2e/rarocrud.pesquisarUsuario.cy.js
@@ -4,6 +4,7 @@ describe("Pesquisar Usuario", function () {
   const name = "testeraro";
   const email = faker.internet.email();
   let emailCadastrado;
+  let nomeCadastrado;
   var paginaRegistro = new CadastroPage();
   beforeEach(function () {
     cy.visit("/users");
@@ -12,11 +13,18 @@ describe("Pesquisar Usuario", function () {
     paginaRegistro.typeEmail(email);
     paginaRegistro.clikButtonSalvar();
     emailCadastrado = email;
+    nomeCadastrado = name;
   });
   it("Deve ser possivel pesquisar usuario pelo email", function () {
     cy.visit("/users");
     cy.get(".sc-dcJsrY.hcYPTs").should("be.visible");
     cy.get(".sc-dcJsrY.hcYPTs").type(email);
-    cy.get("#userDataDetalhe").should("be.visible");
+    cy.wait(5000);
+    cy.get(".sc-dAbbOL.lcgSvJ [data-test='userDataName']").contains(
+      nomeCadastrado
+    );
+    cy.get(".sc-dAbbOL.lcgSvJ [data-test='userDataEmail']").contains(
+      emailCadastrado
+    );
   });
 });
